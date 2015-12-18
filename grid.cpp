987021380250979d04a2c9b1d25b7c837ea69eb0
AkosSzabacsi <akos.szabacsi@gmail.com>
2015-12-18 14:54:13 +0100

    boundary conditions are set

--- grid.cpp
+++ grid.cpp
@@ -7,13 +7,14 @@ struct Point
 {
 	uint x;
 	uint y;
-	uint value;
+	float value;
 	
 	Point(uint _x, uint _y, float _value = 0):
 	x(_x),
 	y(_y),
-	data(_data)
+	value(_value)
 	{}
+	
 };
 
 class Grid
@@ -42,14 +43,16 @@ class Grid
 				for(uint j = 0; j < c; j++)
 				{
 					uint idx = c * i + j;
-					std::cout << "(" << M[idx].x << ", " << M[idx].y << ") ";
+					//std::cout << "(" << M[idx].x << ", " << M[idx].y << ") ";
+					std::cout << M[idx].value << " ";
+					
 				}
 				
 				std::cout << std::endl;
 			}
 		}
 		
-		Point getElement(uint row, uint col)
+		Point& getElement(uint row, uint col)
 		{
 			return M[row * c + col];
 		}
@@ -57,8 +60,23 @@ class Grid
 
 int main()
 {
-	Grid m(10, 10);
+	const uint rows = 10;
+	const uint cols = 10;
+	
+	Grid m(rows, cols);
+	
 	m.print();
 	
-	std::cout << m.getElement(5,5).x << " " << m.getElement(5,5).y << std::endl;
+	for(uint i = 0; i < rows; i++)
+	{
+		for(uint j = 0; j < cols; j++)
+		{
+			m.getElement(i, 0).value = 1;
+			m.getElement(0, j).value = 2;
+			m.getElement(i, cols-1).value = 3;
+			m.getElement(rows-1, j).value = 4;
+		}
+	}
+	
+	m.print();
 }
